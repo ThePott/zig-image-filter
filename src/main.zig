7de7653c4ef04c781e1ab27c9d490c2d3f506073
Tynahan <nusilite@gmail.com>
2026-05-05 15:42:31 +0900

    feat: calc decoded image size

--- src/main.zig
+++ src/main.zig
@@ -2,11 +2,22 @@ const std = @import("std");
 const libspng = @import("libspng");
 const expect = std.testing.expect;
 
-fn getImageHeader(spng_context_pointer: *libspng.spng_ctx) !void {
+const SpngError = error{ FailedToGetImageHeader, FailedToCalcDecodedSize };
+
+fn getImageHeader(spng_context: *libspng.spng_ctx) SpngError!libspng.spng_ihdr {
     var image_header: libspng.spng_ihdr = undefined;
-    const image_header_result = libspng.spng_get_ihdr(spng_context_pointer, &image_header);
-    std.debug.print("image header result: {any}\n", .{image_header_result});
-    if (image_header_result != 0) @panic("FAILED TO GET IMAGE HEADER");
+    const image_header_result = libspng.spng_get_ihdr(spng_context, &image_header);
+    if (image_header_result != 0) return SpngError.FailedToGetImageHeader;
+
+    return image_header;
+}
+
+fn calcDecodedImageSize(spng_context: *libspng.spng_ctx) usize {
+    var output_size: usize = undefined;
+    const output_size_result = libspng.spng_decoded_image_size(spng_context, libspng.SPNG_FMT_RGBA8, &output_size);
+    if (output_size_result != 0) return SpngError.FailedToCalcDecodedSize;
+
+    return output_size;
 }
 
 pub fn main() !void {
@@ -20,12 +31,14 @@ pub fn main() !void {
         if (file_close_result != 0) @panic("FAILED TO CLOSE FILE");
     }
 
-    std.debug.print("image file: {any}\n", .{image_file});
-
     // TODO: 도대체 뭐가 0이냐
     const spng_context = libspng.spng_ctx_new(0) orelse unreachable;
     const set_png_file_result = libspng.spng_set_png_file(spng_context, image_file);
     std.debug.print("set png file result: {any}\n", .{set_png_file_result});
 
-    try getImageHeader(spng_context);
+    const image_header = try getImageHeader(spng_context);
+    std.debug.print("image header: {any}\n", .{image_header});
+
+    const decoded_image_size = calcDecodedImageSize(spng_context);
+    std.debug.print("decoded image size: {any}\n", .{decoded_image_size});
 }
